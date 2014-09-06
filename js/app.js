83b23d7ce98dd5208d00dc1dd595a6eeeae53abe
CallmeSurge <bello.a.babajide@gmail.com>
2014-09-06 10:40:29 +0100

    change

--- js/app.js
+++ js/app.js
@@ -1,33 +1,61 @@
 // generating a random number from an array of [0-100]
+var enternumber;
+var initNum=0;
 
-var randomnum=Math.ceil(Math.random()*100);
-var hotorcold= function(guess){
+$('#restart').click(function(event){
+	location.reload();
+});	
 
-	// while (guess !== randomnum) {
-		console.log('randomnum: ' + randomnum);
+
+var randomNum=Math.ceil(Math.random()*100);
+
+// checking for the validity of the number inputed
+
+
+var numValidity= function(){
+
+		enternumber = $("#enternumber").val();
 		
-		if(isNaN(guess)){
-			console.log("You have to input a number");
+		if((isNaN(enternumber)) || (enternumber === "")){
+			$("#statement p").html("Your have to input a number to play");
 		}
-		else if(guess===randomnum){
-			console.log("You are right");
+		else if(enternumber===null){
+			$("#statement p").html("You have to input a number");
 		}
-		else if(guess>100){
-			console.log("Your number is out of range")
+		else if(0>enternumber){
+			$("#statement p").html("You have to input a positive number");
 		}
-		else if(guess<randomnum){
-			console.log("Your number is less");
+		else if(enternumber>100){
+			$("#statement p").html("Your input is out of range");
 		}
-		else if(guess>randomnum){
-			console.log("Your number is more-than");
+		else{
+			hotorcold();
 		}
-	// }
 }
 
 
+
+// the main operation
+
+var hotorcold= function(){
+
+		enternumber = parseInt(enternumber)
+
+		if(enternumber == randomNum){
+			$("#statement p").html("You are Correct");
+		}
+		else if(Math.abs(randomNum - enternumber) > Math.abs(randomNum - initNum)){
+			$("#statement p").html("Your number is colder");
+		}
+		else if(Math.abs(randomNum - enternumber) < Math.abs(randomNum - initNum)){
+			$("#statement p").html("Your number is hotter");
+		}
+
+		initNum = enternumber
+}
+console.log('randomNum: ' + randomNum);
+
 $('#submit').click(function(event) {
 	event.preventDefault;
-	var num = $('#enternumber').val();
-	console.log('num: ' + num);
-	hotorcold(num);
+	numValidity();
 });
